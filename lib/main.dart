f77fb8b13c19959ab81e218d3023fdc147c8e676
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-06-04 23:20:54 +0200

    removed some and renamed some, fixed heigt tho

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' as foundation;
-import 'dart:async';
 
 bool safemode = false;
 
@@ -10,8 +8,8 @@ void main() {
       initialRoute: '/',
       routes: {
         '/': (context) => const Homescreen(),
-        '/second': (context) => const Flashing(),
-        //'/thirt': (context) => const Settings(),
+        '/settings': (context) => const settings(),
+        '/thirt': (context) => const settings(),
       },
     ),
   );
@@ -21,83 +19,53 @@ class Homescreen extends StatelessWidget {
   const Homescreen({key});
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        height: MediaQuery.of(context).size.width,
-        width: 500,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Home Screen"),
-          ),
-          body: Center(
-              child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/second');
-                },
-                child: const Text('Waiting time'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/thirt');
-                },
-                child: const Text('settings'),
-              ),
-            ],
-          )),
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Screen"),
         ),
-      )
-    ]);
+        body: Center(
+            child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/thirt');
+              },
+              child: const Text('Waiting time'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/settings');
+              },
+              child: const Text('settings'),
+            ),
+          ],
+        )),
+      ),
+    );
   }
 }
 
-class Flashing extends StatefulWidget {
-  const Flashing({key});
+class settings extends StatefulWidget {
+  const settings({Key? key}) : super(key: key);
 
   @override
-  State<Flashing> createState() => _FlashingState();
+  State<settings> createState() => _settingsState();
 }
 
-class _FlashingState extends State<Flashing> {
-  bool _isNear = false;
-  bool opacity = false;
-  late StreamSubscription<dynamic> _streamSubscription;
-
+class _settingsState extends State<settings> {
   @override
-  void initState() {
-    super.initState();
-    listenSensor();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _streamSubscription.cancel();
-  }
-
-  Future<void> listenSensor() async {
-    FlutterError.onError = (FlutterErrorDetails details) {
-      if (foundation.kDebugMode) {
-        FlutterError.dumpErrorToConsole(details);
-      }
-    };
-  }
-
   Widget build(BuildContext context) {
-    return Opacity(
-        opacity: opacity ? 0.0 : 1.0,
-        child: GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: () => Navigator.pop(context),
-          child: Scaffold(
-              body: Image.asset(
-            "assets/images/flash.gif",
-            fit: BoxFit.cover,
-            height: double.infinity,
-            width: double.infinity,
-            alignment: Alignment.center,
-          )),
-        ));
+    return Container(
+        height: MediaQuery.of(context).size.width,
+        width: MediaQuery.of(context).size.height,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("Settings"),
+            ),
+            body: Center(
+                child: Column(
+              children: [],
+            ))));
   }
 }
