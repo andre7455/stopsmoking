059ad5fb183c3156022cc4470d16693dadb54cb6
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-08-09 16:10:41 +0200

    Added some comments
    added some commends to understand what is happening in this example

--- lib/example.dart
+++ lib/example.dart
@@ -1,25 +1,17 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: public_member_api_docs
-
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const example());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+//default starter class bullshit
+class example extends StatelessWidget {
+  const example({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'SharedPreferences Demo',
       home: SharedPreferencesDemo(),
     );
   }
